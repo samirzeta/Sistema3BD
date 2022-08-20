477540effb4cb1453e392b7017dc4704c7cb9043
samirzeta <samirzetalmlsak@gmail.com>
2022-08-20 13:36:50 -0500

    samirztaa

--- samirzta.php
+++ samirzta.php
@@ -1,2 +1,9 @@
-<?php 
- ?>
+<!--<div class="form-group col-md-3">
+    <label for="curso">Curso</label>
+    <select type="text" id="idCurso" name="idCurso" value="" class="form-control formulario__input" placeholder=""  required>
+            <?php
+            /*while($row = $stmt->fetch(PDO::FETCH_ASSOC)) { ?>
+            <option value="<?php echo $row['nombre']; ?>"><?php echo $row['nombre']; ?></option>
+        <?php } ?>
+    </select>
+</div>-->
